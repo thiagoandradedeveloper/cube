5bcc6c8de35038ff8dd8a184f219e5cf82deaf8d
thiagoandradedeveloper <thiagoha3@gmail.com>
2024-01-30 14:05:54 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+
+    let prev = document.querySelector('.prev');
+    let next = document.querySelector('.next');
+    let box  = document.querySelector('.box');
+    let degree = 0;
+    
+    prev.addEventListener('click',function(){
+        degree += 90;
+        box.style = `transform: perspective(1000px) rotateY(${degree}deg);`;
+    })
+    next.addEventListener('click',function(){
+        degree -= 90;
+        box.style = `transform: perspective(1000px) rotateY(${degree}deg);`;
+    })
+}
