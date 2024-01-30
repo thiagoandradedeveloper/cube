9d69cfe0ed2813b1374831d09cdd91b045dadd76
thiagoandradedeveloper <thiagoha3@gmail.com>
2024-01-30 14:27:10 -0300

    melhoramento

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 window.onload = function(){
 
-    let prev = document.querySelector('.prev');
-    let next = document.querySelector('.next');
-    let box  = document.querySelector('.box');
+    let prev  = document.querySelector('.prev');
+    let next  = document.querySelector('.next');
+    let box   = document.querySelector('.box');
+    let dados = document.querySelector('#dados')
     let degree = 0;
     
     prev.addEventListener('click',function(){
@@ -11,6 +12,13 @@ window.onload = function(){
     })
     next.addEventListener('click',function(){
         degree -= 90;
+        box.style = `transform: perspective(1000px) rotateY(${degree}deg);`;
+    })
+    dados.addEventListener('mouseup',function(){
+        var valorFracionado = Math.random();
+        var valorFinal = Math.floor(valorFracionado * (7 - 3 + 1)) + 3;
+
+        degree += 90 * valorFinal;
         box.style = `transform: perspective(1000px) rotateY(${degree}deg);`;
     })
 }
